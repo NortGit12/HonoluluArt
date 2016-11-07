a4704f75f50b9bde655108f653fe177a22dedf04
Jeff Norton <jeff12ntn@gmail.com>
2016-11-07 15:02:33 -0700

    initial view setup

---
+++ HonoluluArt/MapViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MapViewController.swift
+//  HonoluluArt
+//
+//  Created by Jeff Norton on 11/7/16.
+//  Copyright © 2016 JeffCryst. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - _Properties
+    //==================================================
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //==================================================
+    // MARK: - General
+    //==================================================
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
